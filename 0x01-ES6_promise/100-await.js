8a73f4568f96a58ef28b0eefe55681ae5c8e67e5
Maylebuh <malebomawela96@gmail.com>
2024-07-25 09:17:11 -0700

    100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let photo;
-  let user;
+  let res = {};
+
   try {
-    photo = await uploadPhoto();
-    user = await createUser();
-  } catch (error) {
-    photo = null;
-    user = null;
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
-  return { photo, user };
+  return res;
 }
